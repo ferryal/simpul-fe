16997c6d8d70f7d254e1dd02be7fd4dbfa414d02
Ferryal Fajar <ferryal21@gmail.com>
2024-08-23 19:27:33 +0700

    feat: add icon

---
+++ src/icons/arrow-downward-icon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const ArrowDownwardIcon: React.FC = () => {
+	return (
+		<svg
+			width="16"
+			height="16"
+			viewBox="0 0 16 16"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M13.3334 8L12.3934 7.06L8.66675 10.78V2.66667H7.33341V10.78L3.61341 7.05334L2.66675 8L8.00008 13.3333L13.3334 8Z"
+				fill="#F2F2F2"
+			/>
+		</svg>
+	);
+};
